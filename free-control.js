a55b5f1770a8a47a597597dcc7238b63458bc6dd
anthony_numbate <anthony@numbate.com>
2014-11-20 20:45:27 +0100

    Added sendDigit() method

--- free-control.js
+++ free-control.js
@@ -2,7 +2,7 @@ var FreeControl = function(code){
 
     var path = 'http://hd1.freebox.fr/pub/remote_control?code={code}&key={key}&long={long}&repeat={repeat}';
 
-    function sendCommand(key, long, repeat){
+    function sendCommand(key, long, repeat, callback){
         
         var xhr     = new XMLHttpRequest(),
             address = path
@@ -15,16 +15,34 @@ var FreeControl = function(code){
         
         xhr.onreadystatechange = function() {
             if(xhr.readyState==4){
-                console.log('done');
+                if( 'function' === typeof callback) callback();
             }
         }
         xhr.send();
     }
 
+    function sendDigit(digit){
+        var digit   = digit.toString(),
+            length  = digit.length,
+            current = 0;
+        function _rec(key){
+            sendCommand(key, false, false, function(){
+                current++;
+                if(current <= length){
+                    rec(digit[current]);
+                }
+            });
+        }
+
+        _rec(digit[0]);
+
+    }
+
+
     return {
 
         channel:function(channel){
-            sendCommand(channel);
+            sendDigit(channel);
         }
 
     };
